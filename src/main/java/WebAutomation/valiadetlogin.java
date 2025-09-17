75305705217845f9e8e49fd4cd851c5bd6586cbf
omdeshmukh98004 <omdeshmukh98004@gmail.com>
2025-09-17 09:29:19 +0530

    new code

--- src/main/java/WebAutomation/valiadetlogin.java
+++ src/main/java/WebAutomation/valiadetlogin.java
@@ -1,9 +1,11 @@
 package WebAutomation;
 
+import org.jspecify.annotations.Nullable;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class valiadetlogin {
@@ -15,7 +17,11 @@ public class valiadetlogin {
 		driver.manage().window().maximize();
 		driver.get("https://www.puneripattern.com/");
 		driver.findElement(By.xpath("(//button[@type='button'])[7]")).click();
-		
+	@Nullable
+	String ActualTitle = driver.getTitle();
+	System.out.println(ActualTitle);
+	Assert.assertEquals(ActualTitle,"123");
+	System.out.println("succesfully landed to the home page");
 	}
 
 }
